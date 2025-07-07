ada49afde99ffebb7ef08bf0c0c7d7ccc742b7d2
islamemon59 <islamemon59@gmail.com>
2025-07-07 22:13:47 +0600

    useAuth useAxiosSecure imageUploadApi added

---
+++ src/Hooks/useAxiosSecure.jsx
@@ -0,0 +1,49 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router";
+import useAuth from "./useAuth";
+
+const axiosSecure = axios.create({
+  baseURL: import.meta.env.VITE_URL,
+});
+
+const useAxiosSecure = () => {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    axiosSecure.interceptors.request.use(
+      (config) => {
+        config.headers.authorization = `Bearer ${user?.accessToken}`;
+        return config;
+      },
+      (error) => {
+        return Promise.reject(error);
+      }
+    );
+    axiosSecure.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        if (error.response) {
+          const status = error.response.status;
+
+          if (status === 401) {
+            // navigate("/login")
+            console.error("Unauthorized! Token might be invalid or expired.");
+          } else if (status === 403) {
+            navigate("/forbidden");
+            console.error("Forbidden! You don't have permission.");
+          } else if (status >= 500) {
+            console.error("Server error! Please try again later.");
+          }
+        }
+        return Promise.reject(error);
+      }
+    );
+  }, [navigate, user?.accessToken]);
+
+  return axiosSecure;
+};
+
+export default useAxiosSecure;
