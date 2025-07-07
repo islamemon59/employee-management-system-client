4edc70528c883c96b233180e4e1e3b458d614c1f
islamemon59 <islamemon59@gmail.com>
2025-07-08 01:17:36 +0600

    navbar login registe footer component added

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,9 +1,12 @@
+
 import React from 'react';
 import { Outlet } from 'react-router';
+import Navbar from '../Shared/Navbar/Navbar';
 
 const MainLayout = () => {
     return (
         <div>
+            <Navbar/>
             <Outlet/>
         </div>
     );
