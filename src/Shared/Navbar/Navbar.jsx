bf135512b251cf0eaf829fce849c020eed51ba63
islamemon59 <islamemon59@gmail.com>
2025-07-13 15:37:13 +0600

    unusual code deleted

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import NavbarLinks from "../../Components/NavbarLinks/NavbarLinks";
 import useAuth from "../../Hooks/useAuth";
